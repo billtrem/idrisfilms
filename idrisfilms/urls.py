ba167a0c5535fa85ba4a7236fc601ff6efbf74d3
billtrem <wsf.tremlett@gmail.com>
2025-08-29 02:43:38 +0100

    Fix URL routing and template render

--- idrisfilms/urls.py
+++ idrisfilms/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('idrisfilms_site.urls')),  # connect app urls
+    path("admin/", admin.site.urls),
+    path("", include("idrisfilms_site.urls")),  # ✅ point to app urls
 ]
